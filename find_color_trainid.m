162f6ace42ed512cf65d3e4608f23e35b73b5095
muralabmahmuds <32534483+muralabmahmuds@users.noreply.github.com>
2020-12-04 07:58:15 +0900

    Update find_color_trainid.m

--- find_color_trainid.m
+++ find_color_trainid.m
@@ -6,7 +6,7 @@ function out = find_color_trainid(labels, rr, gg, bb)
 % trainid = 2
 % rr, gg, bb = 4, 5, 6
 
-out = 99;   % a marker when an error color may accur
+out = 255;   % a marker when an error color may accur, 255: ignored in trainid
 for ii=1:size(labels,1)
     if (labels(ii,4)==rr && labels(ii,5)==gg && labels(ii,6)==bb)
         out = labels(ii,2);
