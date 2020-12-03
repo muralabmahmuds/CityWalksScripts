462f115b715e6b7ba4ce6de28e58ae48a637d8a2
muralabmahmuds <32534483+muralabmahmuds@users.noreply.github.com>
2020-12-04 07:55:51 +0900

    Update find_color_labelid.m

--- find_color_labelid.m
+++ find_color_labelid.m
@@ -6,7 +6,7 @@ function out = find_color_labelid(labels, rr, gg, bb)
 % labelid = 1
 % rr, gg, bb = 4, 5, 6
 
-out = 99;   % a marker when an error color may accur
+out = 0;   % a marker when an error color may accur, 0: ignored in labelid
 for ii=1:size(labels,1)
     if (labels(ii,4)==rr && labels(ii,5)==gg && labels(ii,6)==bb)
         out = labels(ii,1);
